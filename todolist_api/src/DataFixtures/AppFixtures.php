fd73629cf62d52eb06200f77f31966c5edbd4b0b
amjed96 <amjedbouallegui@gmail.com>
2024-01-12 12:34:30 +0100

    add user entity and autentication

---
+++ todolist_api/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
+class AppFixtures extends Fixture
+{
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $user = new User();
+        $user->setUsername("userone");
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
+        $manager->persist($user);
+
+        $user2 = new User();
+        $user2->setUsername("usertwo");
+        $user2->setRoles(["ROLE_USER"]);
+        $user2->setPassword($this->userPasswordHasher->hashPassword($user2, "password"));
+        $manager->persist($user2);
+
+        $manager->flush();
+    }
+}
